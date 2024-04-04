7a9b0fbe39543dab4b36f39ce1748b070b7e6de7
theulins <matheuslins1607@gmail.com>
2024-04-03 21:31:32 -0300

    teste

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
-    <title>Login</title>
+    <title>Logi</title>
 </head>
 
 <body>
